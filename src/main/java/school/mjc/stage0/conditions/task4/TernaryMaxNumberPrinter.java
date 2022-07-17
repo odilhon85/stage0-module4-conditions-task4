b1c2ce7e907aecfca88b532fc9a2a0da4043de9e
odilhon85 <i.o.r@inbox.ru>
2022-07-17 11:09:12 +0500

    Changes done 1-attempt

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int firstGreates = first > second ? first : second;
+        int secondGreatest = second > third ? second : third;
+
+        System.out.println(firstGreates > secondGreatest ? firstGreates : secondGreatest);
     }
 }
